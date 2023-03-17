79ac4b575f6ebbd057a5baa62796436ff093bb85
l3miage-ouchanes <souleymen@ouchane.fr>
2023-03-17 11:56:05 +0100

    Mapping Client.java (manque le mapping de la liste de prénoms et de noms)

--- data/src/main/java/fr/uga/l3miage/photonum/data/domain/Client.java
+++ data/src/main/java/fr/uga/l3miage/photonum/data/domain/Client.java
@@ -3,13 +3,25 @@ package fr.uga.l3miage.photonum.data.domain;
 import java.util.List;
 import java.util.Set;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Client")
 public class Client {
-    private Long id;
+    @Id
+    @Column(name = "email")
+    private String email; // c'est le mail qui identifie un client (mail unique)
+
+    ////////////// jsp quoi mettre !!!!!!!!!!!!!!!!!!!!!!!!!!!!
     private List<String> prenom;
     private List<String> nom;
-    private String email;
+
+    @Column(name = "mdp")
     private String mdp;
+
+    @ManyToMany
     private Set<Adresse> adresses;
+
 
     public Set<Adresse> getAdresses() {
         return adresses;
@@ -49,14 +61,5 @@ public class Client {
 
     public void setMdp(String mdp) {
         this.mdp = mdp;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
+    }  
 }
