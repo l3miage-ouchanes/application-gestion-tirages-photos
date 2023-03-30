d52501a9f0170cb86e1ce952ef81b7195f69e0e5
l3miage-oulahcel <oulahcel@etu.univ-grenoble-alpes.fr>
2023-03-30 10:34:03 +0200

    Service Repository Image

--- service-impl/src/main/java/fr/uga/l3miage/photonum/service/ImageServiceImpl.java
+++ service-impl/src/main/java/fr/uga/l3miage/photonum/service/ImageServiceImpl.java
@@ -1,7 +1,47 @@
 package fr.uga.l3miage.photonum.service;
 
-public class ImageServiceImpl {
-    
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.uga.l3miage.photonum.data.domain.Image;
+import fr.uga.l3miage.photonum.data.repo.ImageRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class ImageServiceImpl  implements ImageService {
+
+
+    private final ImageRepository imageRepository;
+
+    @Autowired
+    public ImageServiceImpl(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+
+    @Override
+    public Image save(Image image) {
+        return imageRepository.save(image);
+    }
+
+    @Override
+    public Image get(Long id) throws EntityNotFoundException {
+        return imageRepository.get(id);
+    }
+
+    @Override
+    public Collection<Image> list() {
+        return imageRepository.all();
+    }
+
+    @Override
+    public Image update(Image object) throws EntityNotFoundException {
+        return imageRepository.save(object);
+    }
+
     public void delete(int id) throws EntityNotFoundException, DeleteImageException {
 
     }
